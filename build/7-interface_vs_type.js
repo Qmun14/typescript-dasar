db9ae758529814ac806e71a554cdc7a3709066d6
Qmun14 <mamunramdhan@gmail.com>
2023-03-25 17:29:31 +0700

    Interface vs Type

--- build/7-interface_vs_type.js
+++ build/7-interface_vs_type.js
@@ -1,2 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const mySong = {
+    artisName: "Led Zeppelin",
+    songName: "Stairway to Heaven"
+};
+console.log(mySong);
+const myData = {
+    id: 1,
+    propA: "coba",
+    propB: "cobaB"
+};
+const myData2 = {
+    id: 1,
+    propB: "cobaB"
+};
+class People {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getName(id) {
+        return "Yess Mantap";
+    }
+}
+let myPeople = new People("Ma'mun", 31);
+console.log(myPeople);
+class People2 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.street = "Jalan Raya";
+    }
+    getName(id) {
+        return "Bagus Sekalii";
+    }
+}
--- src/7-interface_vs_type.ts
+++ src/7-interface_vs_type.ts
@@ -1,23 +1,129 @@
 /* eslint-disable */
 
-export {}; // quick fix for global variable
+export { }; // quick fix for global variable
 
 /**
  * Object
  */
+// user --> name, age
+
+interface User {
+    name: string;     // -->> tidak perlu sama dengan (=) cz defaultnya interface itu adalah object
+    age: number;
+}
+
+type TUser = {
+    name: string;
+    age: number;
+}
+
 
 /**
  * Merge
  */
+interface Song {
+    artisName: string;
+}
+
+interface Song {
+    songName: string;
+}
+
+const mySong: Song = {
+    artisName: "Led Zeppelin",
+    songName: "Stairway to Heaven"
+}
+console.log(mySong)
+
+// ?kesimpulannya untuk Merge itu 'type' tidak bisa diimplementasikan
+
 
 /**
  * Intersection & Union
  */
 
+type typeA = {
+    id: number;
+    propA: string;
+}
+
+type typeB = {
+    id: number;
+    propB: string;     //? <------  optional type data
+}
+
+// intersection
+type IntersectionAB = typeA & typeB;
+
+const myData: IntersectionAB = {
+    id: 1,
+    propA: "coba",
+    propB: "cobaB"
+}
+
+// union
+type UnionAB = typeA | typeB;
+
+const myData2: UnionAB = {
+    id: 1,
+    // propA: "coba",
+    propB: "cobaB"
+}
+// ?kesimpulannya untuk  Intersection & Union itu 'Interface' tidak bisa diimplementasikan
+
 /**
  * Implements
  */
+interface Person {      // todo:  untuk Implementns ini bisa menggunakan type juga eg:: -->  type Person = {}
+    name: string;
+    age: number;
+    getName(id: number): string;
+}
+class People implements Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getName(id: number) {
+        return "Yess Mantap";
+    }
+}
+
+let myPeople = new People("Ma'mun", 31);
+console.log(myPeople)
 
 /**
  * Extend
  */
+
+interface Address {
+    street: string;
+}
+
+interface Person2 extends Address {
+    name: string;
+    age: number;
+    getName(id: number): string;
+}
+
+class People2 implements Person2 {
+    name: string;
+    age: number;
+    street: string;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+        this.street = "Jalan Raya"
+    }
+
+    getName(id: number) {
+        return "Bagus Sekalii"
+    }
+
+}
+// ?kesimpulannya untuk  Extends itu 'type' tidak bisa diimplementasikan
